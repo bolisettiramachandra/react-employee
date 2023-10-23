60751affef6443731d0e42defd135d5b9048b058
Rama Chandra Bolisetti <rama.chandra.bolisetti@ibm.com>
2023-10-23 15:37:12 +0530

    Employee app with add, edit , delete employee

--- src/App.js
+++ src/App.js
@@ -1,22 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Employee from "./components/Employee";
+import react, { useState } from "react";
+import AddEmployee from "./components/AddEmployee";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
+  const [role, setRole] = useState("dev");
+  const [employee, setEmployee] = useState([
+    {
+      id: 1,
+      name: "Ram",
+      role: "CEO",
+      img: "https://images.pexels.com/photos/17986565/pexels-photo-17986565/free-photo-of-man-sitting-on-a-chair-on-a-beach-playing-his-guitar.png?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    },
+    {
+      id: 2,
+      name: "Kiran",
+      role: "Manager",
+      img: "https://images.pexels.com/photos/17986565/pexels-photo-17986565/free-photo-of-man-sitting-on-a-chair-on-a-beach-playing-his-guitar.png?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    },
+    {
+      id: 3,
+      name: "Sai",
+      role: "Dev",
+      img: "https://images.pexels.com/photos/17986565/pexels-photo-17986565/free-photo-of-man-sitting-on-a-chair-on-a-beach-playing-his-guitar.png?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    },
+    {
+      id: 4,
+      name: "nav",
+      role: "Dev",
+      img: "https://images.pexels.com/photos/17986565/pexels-photo-17986565/free-photo-of-man-sitting-on-a-chair-on-a-beach-playing-his-guitar.png?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    },
+    {
+      id: 5,
+      name: "Anil",
+      role: "Dev",
+      img: "https://images.pexels.com/photos/17986565/pexels-photo-17986565/free-photo-of-man-sitting-on-a-chair-on-a-beach-playing-his-guitar.png?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    },
+    {
+      id: 6,
+      name: "Ajay",
+      role: "Tester",
+      img: "https://images.pexels.com/photos/17986565/pexels-photo-17986565/free-photo-of-man-sitting-on-a-chair-on-a-beach-playing-his-guitar.png?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    },
+    {
+      id: 7,
+      name: "Kiran",
+      role: "UI / UX",
+      img: "https://images.pexels.com/photos/17986565/pexels-photo-17986565/free-photo-of-man-sitting-on-a-chair-on-a-beach-playing-his-guitar.png?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    },
+    {
+      id: 8,
+      name: "Ganesh",
+      role: "Youtuber",
+      img: "https://images.pexels.com/photos/17986565/pexels-photo-17986565/free-photo-of-man-sitting-on-a-chair-on-a-beach-playing-his-guitar.png?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    },
+  ]);
+
+  function updateEmployee(id, newName, newRole) {
+    const updatedEmployees = employee.map((employee) => {
+      if (id === employee.id) {
+        return { ...employee, name: newName, role: newRole };
+      }
+      return employee;
+    });
+    setEmployee(updatedEmployees);
+  }
+
+  function newEmployee(name, role, img) {
+    const newEmployee = {
+      id: uuidv4(),
+      name: name,
+      role: role,
+      img: img,
+    };
+    setEmployee([...employee, newEmployee]);
+  }
+
+  function removeEmployee(id) {
+    const newList = employee.filter((item) => item.id !== id);
+
+    setEmployee(newList);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello Ram !!!!!!!!</p>
+        <input
+          type="text"
+          onChange={(e) => {
+            console.log(e.target.value);
+            setRole(e.target.value);
+          }}
+        ></input>
+        <div className="justify-center flex flex-wrap">
+          {employee.map((emp) => {
+            return (
+              <Employee
+                key={emp.id}
+                id={emp.id}
+                name={emp.name}
+                role={emp.role}
+                img={emp.img}
+                updateEmployee={updateEmployee}
+                removeEmployee={removeEmployee}
+              />
+            );
+          })}
+        </div>
+        <AddEmployee newEmployee={newEmployee} />
       </header>
     </div>
   );
---
+++ src/components/Employee.js
@@ -0,0 +1,37 @@
+import EditEmployee from "./EditEmployee";
+
+function Employee(props) {
+  return (
+    <div className="m-1 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+      <img
+        className="object-cover rounded-full h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
+        src={props.img}
+      />
+      <div className="text-center space-y-2 sm:text-left">
+        <div className="space-y-0.5">
+          <p className="text-lg text-black font-semibold">{props.name}</p>
+          <p className="text-slate-500 font-medium">{props.role}</p>
+        </div>
+        <EditEmployee
+          id={props.id}
+          name={props.name}
+          role={props.role}
+          updateEmployee={props.updateEmployee}
+        />
+
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={(e) => {
+            e.preventDefault();
+            console.log("remove button clicked");
+            props.removeEmployee(props.id);
+          }}
+        >
+          Remove
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Employee;
